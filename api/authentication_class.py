34b7d4ee1eff4b5f5fdb44d03b21884c0c298062
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-03-25 06:31:10 +0200

    adding the utility for signing in username or password

---
+++ api/authentication_class.py
@@ -0,0 +1,29 @@
+'''authentications backends'''
+from rest_framework.authentication import SessionAuthentication
+from django.contrib.auth import get_user_model
+from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
+
+
+class CsrfExemptSessionAuthentication(SessionAuthentication):
+    ''' To not perform the csrf check previously happening'''
+
+    def enforce_csrf(self, request):
+        return
+
+
+class EmailOrUserNameModelBackend(ModelBackend):
+    '''Authenticate with username or password'''
+
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        user_model = get_user_model()
+        if username is not None:
+            users = user_model.objects.filter(
+                Q(username__iexact=username) | Q(email__iexact=username))
+
+        for user in users:
+            if user.check_password(password):
+                return user
+        if not users:
+            # this line for reducing time between existing user and none existing user
+            user_model().set_password(password)
