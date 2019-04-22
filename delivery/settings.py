343036189930eb7a0eb5c181ee708b9018617362
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-04-23 01:39:56 +0200

    updating to use postgres database

--- delivery/settings.py
+++ delivery/settings.py
@@ -84,8 +84,15 @@ WSGI_APPLICATION = 'delivery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': config.get("database", "DB_NAME"),
+        'USER': config.get("database", "DB_USER"),
+        'PASSWORD': config.get("database", "DB_PASSWORD"),
+        'HOST': 'LOCALHOST',
+        'PORT': '5432'
+
+
+
     }
 }
 
