daa87b0777eff06a6adac2cdfd3535f9342b677f
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-04-30 17:12:55 +0200

    fixing importing

--- api/serializers/__init__.py
+++ api/serializers/__init__.py
@@ -1,3 +1,3 @@
-from .package_serializer import *
-from .token_custom_serializer import *
-from .user_serializer import *
+from .package_serializer import PackageSerializer
+from .token_custom_serializer import AuthTokenCustomSerializer
+from .user_serializer import UserSerializer
