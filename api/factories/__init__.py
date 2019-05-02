3a351ef1a9654951f11b32ad28090f433a5f099e
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-05-02 05:16:58 +0200

    adding factories for user

---
+++ api/factories/__init__.py
@@ -0,0 +1,1 @@
+from .user_factories import ClientFactory, CaptainFactory
---
+++ api/factories/user_factories.py
@@ -0,0 +1,39 @@
+import os
+import factory
+from ..models import User, Captain
+from wassally.settings import MEDIA_ROOT
+import faker
+
+
+class ClientFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    image = factory.django.ImageField(
+        from_path=os.path.join(MEDIA_ROOT, "default.png"),
+        format="png"
+    )
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    username = factory.LazyAttribute(
+        lambda a: '{0}_{1}'.format(a.first_name, a.last_name)
+    )
+    email = factory.LazyAttribute(
+        lambda a: '{0}.{1}@example.com'.format(a.first_name, a.last_name)
+    )
+    is_client = True
+    is_captain = False
+    governate = faker.Faker('ar_EG').country()
+    city = faker.Faker('ar_EG').city()
+    phone_number = factory.Sequence(lambda n: "0101176018{}".format(n))
+
+
+class CaptainFactory(factory.DjangoModelFactory):
+    class Meta:
+        model = Captain
+
+    user = factory.SubFactory(ClientFactory)
+    user.is_captain = True
+    user.is_client = False
+    national_id = factory.Sequence(lambda n: '1111111{}'.format(n))
+    vehicle = factory.Faker('vehicle')
