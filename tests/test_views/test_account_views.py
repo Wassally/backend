abf57795f7c342bfc07fb0014af6e7c04e22dcdb
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-05-05 18:47:44 +0200

    adding test for patch request of account

--- tests/test_views/test_account_views.py
+++ tests/test_views/test_account_views.py
@@ -1,7 +1,9 @@
 from django.urls import reverse
+from django.contrib.auth import get_user_model
 from rest_framework.test import APITestCase
 from rest_framework import status
 from rest_framework.authtoken.models import Token
+from faker import Faker
 from api.factories import ClientFactory, CaptainFactory
 
 
@@ -59,3 +61,37 @@ class AccountTest(APITestCase):
         response = self.client.get(reverse('accounts-list'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 2)
+
+    def test_modify_accounts_as_owner(self):
+        ''' Patch request testing from the owner of the account '''
+        user = get_user_model()
+        fake = Faker()
+        client = ClientFactory()
+        token = Token.objects.create(user=client)
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        data = {
+            "username": "test11",
+            "email": fake.email(),
+            "password": "55",
+            "city": Faker('ar_EG').city(),
+            "governate": Faker('ar_EG').country(),
+            "first_name": fake.first_name(),
+            "last_name": fake.last_name(),
+            "is_client": False,
+            "is_captain": True
+        }
+        response = self.client.patch(
+            reverse('accounts-detail', kwargs={"pk": client.id}),
+            data=data, format='json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(response.json().get('is_client'))
+        self.assertFalse(response.json().get('is_captain'))
+        self.assertEqual(response.json().get('username'), data.get('username'))
+        self.assertEqual(response.json().get('email'), data.get('email'))
+        self.assertEqual(response.json().get('first_name'),
+                         data.get('first_name'))
+        self.assertEqual(response.json().get('city'), data.get('city'))
+        self.assertEqual(response.json().get('governate'),
+                         data.get('governate'))
+        # i will check for password put it will need some research
