9b9a1b421bd364f62425d4455cb60ca24c47a695
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-06-15 22:10:19 +0200

    modyfing package model

--- api/models/package_model.py
+++ api/models/package_model.py
@@ -14,14 +14,7 @@ class Package(models.Model):
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE,
                               related_name="packages")
-    from_governate = models.CharField(max_length=40)
-    from_city = models.CharField(max_length=40)
-    from_address = models.CharField(max_length=40)
-    to_governate = models.CharField(max_length=40)
-    to_city = models.CharField(max_length=40)
-    to_address = models.CharField(max_length=40)
-    receiver_name = models.CharField(max_length=40)
-    receiver_phone_number = models.CharField(max_length=14)
+
     sender_phone_number = models.CharField(max_length=14)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -31,8 +24,7 @@ class Package(models.Model):
     state = models.CharField(choices=s, max_length=10, default="avaliable")
     transport_way = models.CharField(
         choices=t, max_length=9)
-    to_location = models.PointField(srid=4326, null=True, blank=True)
-    from_location = models.PointField(srid=4326, null=True, blank=True)
+
     duration = models.IntegerField()
 
     def __str__(self):
