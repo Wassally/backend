b6692657be1968946d79b603628af556180bdfb2
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-06-27 03:14:46 +0200

    adding compute salary logic

--- api/utils.py
+++ api/utils.py
@@ -1,5 +1,25 @@
+import math
+
+from drf_extra_fields.geo_fields import PointField
+
+from geopy.distance import vincenty
 
 
-def computing_salary(to_place, from_place, weight):
-    if to_place and from_place and weight:
-        return (1)
+def computing_salary(to_location, from_location, weight):
+    ''' function for computin salary '''
+
+    FIXED_TAX = 0
+    RATE = 0
+    to_location = PointField().to_internal_value(to_location)
+    from_location = PointField().to_internal_value(from_location)
+
+    distance = vincenty(to_location.coords, from_location.coords).kilometers
+    distance = math.ceil(distance)
+    if weight < 5:
+        RATE = 1.5
+        FIXED_TAX = 5
+        return (RATE*distance+FIXED_TAX)
+    else:
+        RATE = 1.5
+        FIXED_TAX = 5 + ((weight-5))*0.5
+        return (RATE*distance+FIXED_TAX)
--- api/views/wassally_salary_views.py
+++ api/views/wassally_salary_views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 
 from api.serializers import ComputingSalarySerializer
 
+from api.utils import computing_salary
+
 
 @api_view(['POST'])
 def ComputingSalary(request):
@@ -11,8 +13,11 @@ def ComputingSalary(request):
 
     serializer = ComputingSalarySerializer(data=request.data)
     if serializer.is_valid():
+        from_location = serializer.data['from_location']
+        to_location = serializer.data['to_location']
+        weight = serializer.data['weight']
 
-        salary = 0
+        salary = computing_salary(to_location, from_location, weight)
         content = {"expected_salary": salary}
         return Response(content, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
