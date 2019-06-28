70d68a1704c0b6046f4b911a06fe3e67080e26ea
mahmoudzeyada <mahmoudzeyada440@gmail.com>
2019-06-28 20:34:55 +0200

    changing the salary function

--- api/utils.py
+++ api/utils.py
@@ -5,11 +5,13 @@ from drf_extra_fields.geo_fields import PointField
 from geopy.distance import vincenty
 
 
-def computing_salary(to_location, from_location, weight):
+def computing_salary(to_address, from_address, to_location, from_location, weight):
     ''' function for computin salary '''
 
     FIXED_TAX = 0
     RATE = 0
+    to_governate = to_address.split(",")[-2]
+    from_governate = from_address.split(",")[-2]
     if isinstance(to_location, dict) and isinstance(to_location, dict):
 
         to_location = PointField().to_internal_value(to_location)
@@ -17,11 +19,24 @@ def computing_salary(to_location, from_location, weight):
 
     distance = vincenty(to_location.coords, from_location.coords).kilometers
     distance = math.ceil(distance)
-    if weight < 5:
-        RATE = 1.5
-        FIXED_TAX = 5
-        return (RATE*distance+FIXED_TAX)
+    print(distance)
+
+    if to_governate == from_governate:
+        if weight < 5:
+            RATE = 1.5
+            FIXED_TAX = 5
+
+        else:
+            RATE = 1.60
+            FIXED_TAX = 5 + ((weight-5))*0.5
+
     else:
-        RATE = 1.5
-        FIXED_TAX = 5 + ((weight-5))*0.5
-        return (RATE*distance+FIXED_TAX)
+        if weight < 20:
+            RATE = 1.75
+            FIXED_TAX = 20
+
+        else:
+            RATE = 2
+            FIXED_TAX = 15
+
+    return (round(distance*RATE+FIXED_TAX))
